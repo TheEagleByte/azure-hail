238f4dc85fca6b1ff6f6ca0c356668927c0eac83
EagleByte <28986398+TheEagleByte@users.noreply.github.com>
2020-03-26 10:54:02 -0400

    Update hail-install.sh

--- hail-install.sh
+++ hail-install.sh
@@ -3,8 +3,8 @@
 # Install hail dependencies
 sudo apt-get -y install g++ liblz4-dev
 
-# Create a new python virtual environment called hail using python 3.5
-sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/hail python=3.5 anaconda --yes
+# Create a new python virtual environment called hail using python 3.7
+sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/hail2 python=3.7 anaconda --yes
 
 # Install hail into that new environment
-sudo /usr/bin/anaconda/envs/hail/bin/pip install hail
+sudo /usr/bin/anaconda/envs/hail2/bin/pip install hail
