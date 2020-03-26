e85f99c5ce420d4bbdc098d07937d73084f4ffb2
EagleByte <28986398+TheEagleByte@users.noreply.github.com>
2020-03-26 13:54:40 -0400

    Update set-jupyter-to-hail.sh

--- set-jupyter-to-hail.sh
+++ set-jupyter-to-hail.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set Jupyter Notebook to allow use of this hail environment
-sudo sed -i '/python3_executable_path/c\ \"python3_executable_path\" : \"/usr/bin/anaconda/envs/hail/bin/python3\"' /home/spark/.sparkmagic/config.json
+sudo sed -i '/python3_executable_path/c\ \"python3_executable_path\" : \"/usr/bin/anaconda/envs/hail/bin/python3.7\"' /home/spark/.sparkmagic/config.json
